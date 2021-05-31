a3cb048e182a206a1322300cd41bc8e233d07fac
Diegofergamboa <>
2021-05-31 18:06:34 +0000

    Try before

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 #Write your code below this line 👇
-
-
-
-
-
-
-
-#Write your code above this line 👆
-# Define a function called paint_calc() so that the code below works.   
-
-# 🚨 Don't change the code below 👇
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+total = 0
 
+def paint_calc() :
+    area_total = 0
+    area_total = test_h * test_w
+    number_cans = round(area_total / coverage)
+    print('The number of cans are, '+ str(number_cans))
+
+paint_calc()
 
